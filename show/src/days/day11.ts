9cf6148be8d9f63450ab8c4b2c7994bbf3bb701a
Alex Jercan <jercan_alex27@yahoo.com>
2024-12-11 19:30:58 +0200

    feat: add all days as empty

---
+++ show/src/days/day11.ts
@@ -0,0 +1,144 @@
+import { PartAnimator, Solution, Trace, utils } from "./common";
+
+type Part1TraceItemInput = { kind: "input" };
+
+type Part1TraceItem = Part1TraceItemInput;
+
+class Part1Solution implements Solution<Part1TraceItem> {
+    private input: string;
+
+    setInput(input: string): void {
+        this.input = input;
+    }
+
+    constructor(input: string) {
+        this.input = input;
+    }
+
+    solve(): Trace<Part1TraceItem> {
+        const trace: Trace<Part1TraceItem> = [];
+
+        return trace;
+    }
+}
+
+class Part1Animator implements PartAnimator<Part1TraceItem> {
+    private inputDiv: HTMLDivElement;
+    private solutionDiv: HTMLDivElement;
+
+    constructor(inputDiv: HTMLDivElement, solutionDiv: HTMLDivElement) {
+        this.inputDiv = inputDiv;
+        this.solutionDiv = solutionDiv;
+
+        this.reset();
+    }
+
+    reset(): void {
+        this.inputDiv.classList.remove("hidden");
+        this.solutionDiv.classList.add("hidden");
+        this.solutionDiv.innerHTML = "";
+    }
+
+    begin(): void {
+        this.reset();
+
+        this.create();
+
+        this.inputDiv.classList.add("hidden");
+        this.solutionDiv.classList.remove("hidden");
+    }
+
+    step(step: Part2TraceItem): number {
+        switch (step.kind) {
+        case "input":
+            return this.createInput(step);
+        default:
+            throw new Error(`Unknown step kind: ${(step as Part1TraceItem).kind}`);
+        }
+    }
+
+    private createInput(step: Part1TraceItemInput): number {
+        return 1000;
+    }
+
+    private create(): void {
+    }
+}
+
+type Part2TraceItemInput = { kind: "input" };
+
+type Part2TraceItem = Part2TraceItemInput;
+
+class Part2Solution implements Solution<Part2TraceItem> {
+    private input: string;
+
+    setInput(input: string): void {
+        this.input = input;
+    }
+
+    constructor(input: string) {
+        this.input = input;
+    }
+
+    solve(): Trace<Part2TraceItem> {
+        const trace: Trace<Part2TraceItem> = [];
+
+        return trace;
+    }
+}
+
+class Part2Animator implements PartAnimator<Part2TraceItem> {
+    private inputDiv: HTMLDivElement;
+    private solutionDiv: HTMLDivElement;
+
+    constructor(inputDiv: HTMLDivElement, solutionDiv: HTMLDivElement) {
+        this.inputDiv = inputDiv;
+        this.solutionDiv = solutionDiv;
+
+        this.reset();
+    }
+
+    reset(): void {
+        this.inputDiv.classList.remove("hidden");
+        this.solutionDiv.classList.add("hidden");
+        this.solutionDiv.innerHTML = "";
+    }
+
+    begin(): void {
+        this.reset();
+
+        this.create();
+
+        this.inputDiv.classList.add("hidden");
+        this.solutionDiv.classList.remove("hidden");
+    }
+
+    step(step: Part2TraceItem): number {
+        switch (step.kind) {
+        case "input":
+            return this.createInput(step);
+        default:
+            throw new Error(`Unknown step kind: ${(step as Part2TraceItem).kind}`);
+        }
+    }
+
+    private createInput(step: Part2TraceItemInput): number {
+        return 1000;
+    }
+
+    private create(): void {
+    }
+}
+
+const DESCRIPTION_PART1 = [
+    utils.createParagraph("TODO"),
+];
+
+const DESCRIPTION_PART2 = [
+    utils.createParagraph("TODO"),
+];
+
+const DEFAULT_INPUT_PART1 = "TODO";
+const DEFAULT_INPUT_PART2 = "TODO";
+
+export { Part1Solution, Part1Animator, Part2Solution, Part2Animator, DESCRIPTION_PART1, DESCRIPTION_PART2, DEFAULT_INPUT_PART1, DEFAULT_INPUT_PART2 };
